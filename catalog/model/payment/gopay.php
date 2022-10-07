e30239c60644304a4d9d46028128f39a90bab242
Romulo <romulo.ufsc@gmail.com>
2022-10-07 17:02:32 +0200

    GPY020-173 enable/disable plugin based on restriction of currency, country, shipping method

--- catalog/model/payment/gopay.php
+++ catalog/model/payment/gopay.php
@@ -2,6 +2,7 @@
 namespace Opencart\Catalog\Model\Extension\OpencartGopay\Payment;
 class GoPay extends \Opencart\System\Engine\Model {
 	public function getMethod( array $address ): array {
+
 		return $this->is_available() ? [
 			'code'       => 'gopay',
 			'title'      => $this->model_setting_setting->getValue( 'payment_gopay_title' ),
@@ -27,16 +28,6 @@ class GoPay extends \Opencart\System\Engine\Model {
 
 			$setting = $_config->get( 'gopay_setting' );
 
-			// Check countries.
-			$billing_country = $this->session->data['shipping_address']['iso_code_3'];
-
-			$countries = json_decode( $this->model_setting_setting->getValue( 'payment_gopay_countries' ) );
-			if ( empty( $countries ) || empty( $billing_country ) ||
-				! in_array( $billing_country, (array) $countries, true ) ) {
-				return false;
-			}
-			// end check countries.
-
 			// Check currency matches one of the supported currencies.
 			$currency = $this->session->data['currency'];
 			if ( empty( $currency ) || ! array_key_exists( $currency, $setting['currencies'] ) ) {
@@ -44,10 +35,38 @@ class GoPay extends \Opencart\System\Engine\Model {
 			}
 			// end check currency.
 
-			// Check shipping methods.
-			$shipping_methods = json_decode( $this->model_setting_setting->getValue( 'payment_gopay_shipping_methods' ) );
-			if ( ! in_array( explode( '.', $this->session->data['shipping_method'] )[0], $shipping_methods ) ) {
+			// Check countries.
+			$country = '';
+			if ( ! array_key_exists( 'shipping_address', $this->session->data ) ) {
+				if ( array_key_exists( 'billing_address', $this->session->data ) ) {
+					$country = $this->session->data['billing_address']['iso_code_3'];
+				}
+			} else {
+				$country = $this->session->data['shipping_address']['iso_code_3'];
+			}
+
+			$countries = json_decode( $this->model_setting_setting->getValue( 'payment_gopay_countries' ) );
+			if ( !empty( $country ) && ( empty( $countries ) || !in_array( $country, (array) $countries, true ) ) ) {
 				return false;
+			}
+			// end check countries.
+
+			$all_virtual_downloadable = true;
+			foreach ( $this->cart->getProducts() as $product ) {
+				if ( !$product['download'] ) {
+					$all_virtual_downloadable = false;
+					break;
+				}
+			}
+
+			// Check shipping methods.
+			if ( $this->cart->hasShipping() && !$all_virtual_downloadable ) {
+				$shipping_methods = json_decode( $this->model_setting_setting->getValue( 'payment_gopay_shipping_methods' ) );
+				if ( array_key_exists( 'shipping_method', $this->session->data ) &&
+					!in_array( explode( '.', $this->session->data['shipping_method'] )[0],
+						$shipping_methods ) ) {
+					return false;
+				}
 			}
 			// end check shipping methods.
 		}
