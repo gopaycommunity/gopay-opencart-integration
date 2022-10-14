8fee3cd9284a614770be50afd837595f68482479
Romulo <romulo.ufsc@gmail.com>
2022-10-14 16:31:39 +0200

    GPY020-194 Create admin info page

--- admin/controller/menu/gopay.php
+++ admin/controller/menu/gopay.php
@@ -37,4 +37,24 @@ class GoPay extends \Opencart\System\Engine\Controller
 		}
 
 	}
+
+	/**
+	 * Create info menu.
+	 *
+	 * @since  1.0.0
+	 */
+	public function info(): void {
+		$this->load->language( 'extension/opencart_gopay/menu/gopay' );
+
+		$data = array();
+
+		$data['header']      = $this->load->controller( 'common/header' );
+		$data['column_left'] = $this->load->controller( 'common/column_left' );
+		$data['footer']      = $this->load->controller( 'common/footer' );
+
+		$data['settings_page'] = $this->url->link( 'extension/opencart_gopay/payment/gopay', 'user_token=' .
+			$this->session->data['user_token'] );
+
+		$this->response->setOutput( $this->load->view( 'extension/opencart_gopay/menu/info', $data ) );
+	}
 }
---
+++ admin/language/en-gb/menu/gopay.php
@@ -0,0 +1,13 @@
+<?php
+# Info header
+$_['plugin_name_header'] = 'Plugin Name';
+$_['version_header']     = 'Version';
+$_['description_header'] = 'Description';
+$_['author_header']      = 'Author';
+$_['settings']    = 'Settings';
+
+# Info
+$_['plugin_name'] = 'OpenCart GoPay gateway';
+$_['version']     = '1.0.0';
+$_['description'] = 'OpenCart and GoPay payment gateway integration By GoPay.';
+$_['author']      = 'GoPay';
