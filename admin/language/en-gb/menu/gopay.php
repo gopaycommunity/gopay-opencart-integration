6f64ef51d127ef13becc27abad355fcdbf5d2e88
Romulo <romulo.ufsc@gmail.com>
2022-10-17 16:03:03 +0200

    Add English language for the plugin menu

--- admin/language/en-gb/menu/gopay.php
+++ admin/language/en-gb/menu/gopay.php
@@ -4,10 +4,19 @@ $_['plugin_name_header'] = 'Plugin Name';
 $_['version_header']     = 'Version';
 $_['description_header'] = 'Description';
 $_['author_header']      = 'Author';
-$_['settings']    = 'Settings';
+$_['settings']           = 'Settings';
 
 # Info
 $_['plugin_name'] = 'OpenCart GoPay gateway';
 $_['version']     = '1.0.0';
 $_['description'] = 'OpenCart and GoPay payment gateway integration By GoPay.';
 $_['author']      = 'GoPay';
+
+# Log
+$_['id']             = 'Id';
+$_['order_id']       = 'Order id';
+$_['transaction_id'] = 'Transaction id';
+$_['message']        = 'Message';
+$_['created_at']     = 'Created at';
+$_['log_level']      = 'Log level';
+$_['log']            = 'Log';
