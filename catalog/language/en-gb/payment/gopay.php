36541ae5b85d6888f8f16913b105bd25a7588ec0
Romulo <romulo.ufsc@gmail.com>
2022-10-19 11:46:33 +0200

    GPY020-202 Add confirm button restriction messages

--- catalog/language/en-gb/payment/gopay.php
+++ catalog/language/en-gb/payment/gopay.php
@@ -1,3 +1,7 @@
 <?php
 // Error
 $_['error_order_payment_creation_failed'] = 'Unfortunately your order could not be created on GoPay.';
+
+// Confirm button restriction messages
+$_['country_restriction_message']      = 'GoPay payment gateway plugin disabled for selected Country!';
+$_['subscription_restriction_message'] = 'Products and subscriptions can not be purchased at the same time and only one subscription per checkout is possible.';
