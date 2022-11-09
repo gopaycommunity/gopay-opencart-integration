728d0e0929698b2d32cfd9e1d7c5586d1c63dee1
Romulo <romulo.ufsc@gmail.com>
2022-11-09 15:59:51 +0100

    Change refund message

--- admin/language/en-gb/sale/gopay.php
+++ admin/language/en-gb/sale/gopay.php
@@ -1,3 +1,6 @@
 <?php
 # Refund
-$_['refund'] = 'Refund';
+$_['refund']                       = 'Refund';
+$_['total_to_be_refunded_message'] = 'Amount to be refunded ( Total already refunded = %s )';
+$_['refund_gopay_message']         = 'A total of %s was already refunded from %s';
+$_['error_refund_gopay_message']   = 'Error! Try again.';
