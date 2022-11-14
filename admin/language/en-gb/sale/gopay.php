3e09b8d4928bf62a6c288d4dd0407977b4e0074e
Romulo <romulo.ufsc@gmail.com>
2022-11-14 12:30:39 +0100

    Partial refund restriction message

--- admin/language/en-gb/sale/gopay.php
+++ admin/language/en-gb/sale/gopay.php
@@ -1,6 +1,7 @@
 <?php
 # Refund
-$_['refund']                       = 'Refund';
-$_['total_to_be_refunded_message'] = 'Amount to be refunded ( Total already refunded = %s )';
-$_['refund_gopay_message']         = 'A total of %s was already refunded from %s';
-$_['error_refund_gopay_message']   = 'Error! Try again.';
+$_['refund']                             = 'Refund';
+$_['total_to_be_refunded_message']       = 'Amount to be refunded ( Total already refunded = %s )';
+$_['refund_gopay_message']               = 'A total of %s was already refunded from %s';
+$_['error_refund_gopay_message']         = 'Error! Try again.';
+$_['partial_refund_restriction_message'] = 'You can only issue a partial refund 24 hours after the payment.';
