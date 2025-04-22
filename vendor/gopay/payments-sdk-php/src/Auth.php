05a37b0e061540c31497c6adf3530e8fabedaea1
Tomas Zeman <tomas.zeman@argo22.com>
2025-04-22 14:41:24 +0200

    feat: Remove unnecessary vendor folder

--- vendor/gopay/payments-sdk-php/src/Auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace GoPay;
-
-interface Auth
-{
-    /** @return \GoPay\Token\AccessToken */
-    public function authorize();
-}
--- vendor/gopay/payments-sdk-php/src/Definition/TokenScope.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace GoPay\Definition;
-
-class TokenScope
-{
-    const CREATE_PAYMENT = 'payment-create';
-    const ALL = 'payment-all';
-}
